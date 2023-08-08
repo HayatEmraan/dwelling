c4b2c150d78df08247b4f53a78b9b9f5863b2011
nimairoy <nimairoy718@gmail.com>
2023-08-08 17:18:58 +0600

    navbar proccessing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,50 @@
 import React from "react";
 
+const navlinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+    icon: "<TbMountain />",
+  },
+  {
+    id: 2,
+    name: "about",
+    path: "/about",
+    icon: "<TbMountain />",
+  },
+  {
+    id: 3,
+    name: "service",
+    path: "/service",
+    icon: "<TbMountain />",
+  },
+  {
+    id: 4,
+    name: "blog",
+    path: "/blog",
+    icon: "<TbMountain />",
+  },
+  {
+    id: 5,
+    name: "testimonils",
+    path: "/testimonils",
+    icon: "<TbMountain />",
+  },
+  {
+    id: 6,
+    name: "contact",
+    path: "/contact",
+    icon: "<TbMountain />",
+  },
+];
+
 const HeaderComponent = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>navbars</h2>
+    </div>
+  );
 };
 
 export default HeaderComponent;
