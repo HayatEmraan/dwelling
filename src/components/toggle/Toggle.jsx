702fbee10343f6775090d5f64a32efeb8e7c3266
raju720190r <raju720190r@gmail.com>
2023-08-09 20:26:08 +0600

    some Problem Solved

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -9,17 +9,18 @@ const Toggle = () => {
     setIsToggled((prevState) => !prevState);
   };
   return (
-    <div className="flex justify-between items-center px-5 border border-gray-400 rounded-lg h-20 lg:w-2/3">
+    <div className="flex justify-between items-center px-5 border border-gray-400 rounded-lg h-20 lg:w-1/3">
       <div className="lg:flex">
-        <h2 className="font-bold mr-5">Display total price</h2>
-        <p>Includes all fees,before taxes</p>
+        <h2 className="font-bold">Display total price</h2>
+        <h2 className="hidden lg:block text-gray-500 mx-5">|</h2>
+        <p className="text-gray-500">Includes all fees,before taxes</p>
       </div>
       <div>
-        <button className={`btn ${isToggled ? "" : ""}`} onClick={handleToggle}>
+        <button className={`${isToggled ? "" : ""}`} onClick={handleToggle}>
           {isToggled ? (
-            <input type="checkbox" className="toggle" checked />
+            <input type="checkbox" className="toggle border-none bg-gray-400" checked />
           ) : (
-            <input type="checkbox" className="toggle" />
+            <input type="checkbox" className="toggle  border-none bg-gray-400" />
           )}
         </button>
       </div>
