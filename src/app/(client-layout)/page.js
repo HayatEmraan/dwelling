b0144a2f837027b4a60d64815178d643644d7455
hayatemraan <emraanhasanhayat@gmail.com>
2023-08-09 16:50:17 +0600

    dev testing

--- src/app/(client-layout)/page.js
+++ src/app/(client-layout)/page.js
@@ -1,8 +1,10 @@
 import Cards from "@/components/cards/cards";
+import Toggle from "@/components/toggle/Toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 p-4 space-y-4">
+      <Toggle></Toggle>
       <Cards />
     </main>
   );
