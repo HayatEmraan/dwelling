16944799d65dadcc382bc18e40bdb4391007b26b
hayatemraan <emraanhasanhayat@gmail.com>
2023-08-09 16:17:37 +0600

    card added

--- src/app/(client-layout)/page.js
+++ src/app/(client-layout)/page.js
@@ -1,10 +1,9 @@
-import Cards from '@/Cards/Cards';
-import Image from 'next/image'
+import Cards from "@/components/cards/cards";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Cards></Cards>
+      <Cards />
     </main>
   );
 }
