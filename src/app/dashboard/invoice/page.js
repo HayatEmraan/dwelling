d5dbe62bd81dbd08a9ff1178c882c2f985d28825
unknown <ChowdhuryJoyanti>
2023-08-18 19:54:12 +0600

    invoice add

--- src/app/dashboard/invoice/page.js
+++ src/app/dashboard/invoice/page.js
@@ -1,9 +1,22 @@
+
+import InvoiceListPage from '@/components/dashboard/invoicePage/InvoiceListPage';
+import Pagination from '@/components/dashboard/invoicePage/Pagination';
+import SearchInvoice from '@/components/dashboard/invoicePage/SearchInvoice';
+import Table from '@/components/dashboard/invoicePage/Table';
 import React from 'react';
 
-const InvoicePage = () => {
+const InvoicePage = ({children}) => {
     return (
-        <div>
-            this invoice page
+        <div className='m-5'>
+            <h3 className='text-3xl font-bold p-4 '>Invoice</h3>
+            <div>
+              <InvoiceListPage/>
+              <SearchInvoice></SearchInvoice>
+              <Table></Table>
+              <Pagination></Pagination>
+                {children}
+            </div>
+       
         </div>
     );
 };
