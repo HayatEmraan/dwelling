325d169e73f6e5957e6ddcb81f6dfb88203b91c6
raju720190r <raju720190r@gmail.com>
2023-08-18 20:41:48 +0600

    Service Details

--- src/app/(client-layout)/details/page.js
+++ src/app/(client-layout)/details/page.js
@@ -3,7 +3,7 @@
 const RoomDetails =async () => {
 
   const res = await fetch(
-    "https://github.com/HayatEmraan/dwelling/blob/ruhulamin1/public/fake.json"
+    "https://raw.githubusercontent.com/HayatEmraan/dwelling/raju/public/fake.json"
   );
   const data =await res.json();
   console.log(data)
@@ -17,10 +17,13 @@ const RoomDetails =async () => {
         <h2 className="font-bold text-xl">{data.name}</h2>
         <div className="lg:flex justify-between">
           <div className="lg:flex lg:space-x-5">
-            <p className="font-bold">4.83 . 190 {data.location}</p>
+            <p className="font-bold">
+
+              Ratings-{data.rating}{}
+            </p>
             <p>
-              Superhost-{" "}
-              <span className="font-bold">Kolkata,West bengal,India </span>
+              Superhost-
+              <span className="font-bold">{data.location}</span>
             </p>
           </div>
           <div className="flex space-x-5">
@@ -35,7 +38,7 @@ const RoomDetails =async () => {
           <div className="grid-cols-6">
             <img
               className="rounded"
-              src="https://a0.muscache.com/im/pictures/e25a9b25-fa98-4160-bfd1-039287bf38b6.jpg?im_w=1200"
+              src="https://images.unsplash.com/photo-1691231862377-7e55d4e6a2c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
               alt=""
             />
           </div>
@@ -43,28 +46,28 @@ const RoomDetails =async () => {
             <div className="">
               <img
                 className="rounded object-contain w-full h-full"
-                src="https://a0.muscache.com/im/pictures/62aecbe5-6f23-41d6-8380-793e4216f3a4.jpg?im_w=1200"
+                src="https://images.unsplash.com/photo-1670517007659-0f32403f236c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
                 alt=""
               />
             </div>
             <div className="">
               <img
                 className="rounded object-contain w-full h-full"
-                src="https://a0.muscache.com/im/pictures/62aecbe5-6f23-41d6-8380-793e4216f3a4.jpg?im_w=1200"
+                src="https://images.unsplash.com/photo-1690746138480-1245dc220809?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                 alt=""
               />
             </div>
             <div className="">
               <img
                 className="rounded object-contain w-full h-full"
-                src="https://a0.muscache.com/im/pictures/62aecbe5-6f23-41d6-8380-793e4216f3a4.jpg?im_w=1200"
+                src="https://images.unsplash.com/photo-1691231862377-7e55d4e6a2c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                 alt=""
               />
             </div>
             <div className="">
               <img
                 className="rounded object-contain w-full h-full"
-                src="https://a0.muscache.com/im/pictures/62aecbe5-6f23-41d6-8380-793e4216f3a4.jpg?im_w=1200"
+                src="https://images.unsplash.com/photo-1670517007659-0f32403f236c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
                 alt=""
               />
             </div>
@@ -75,20 +78,10 @@ const RoomDetails =async () => {
       <div className="grid gap-5 lg:grid-cols-2">
         <div>
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-              molestias repellat odit esse ipsa ut, perspiciatis laudantium
-              natus? Sequi corrupti illum cum in sed porro ducimus, numquam
-              distinctio aliquid ratione aliquam ad, aspernatur perferendis
-              suscipit molestiae nisi vero quas iure recusandae placeat ullam
-              ex? Molestiae, optio? Praesentium a sapiente accusantium.
-            </p>
+            <p>{data.description}</p>
           </div>
           <div>
-            <h2 className="font-bold text-xl">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consectetur, ullam.
-            </h2>
+            <h2 className="font-bold text-xl">Category-{data.category}</h2>
           </div>
           <div>
             <h2 className="font-bold">Most Popular Facilities: </h2>
@@ -104,18 +97,18 @@ const RoomDetails =async () => {
         {/* prie and Date */}
         <div className="border rounded shadow-lg p-5">
           <h5>
-            <span className="font-bold mr-2">$Price</span>night
+            <span className="font-bold mr-2">${data.price}</span>night
           </h5>
           <div>
             <div className="border rounded-xl">
               <div className="grid grid-cols-2 p-2 text-center">
                 <div className="border ">
                   <h3>CHECK-IN</h3>
-                  <p>10/10/10</p>
+                  <p>{data.dateRange}</p>
                 </div>
                 <div className="border">
                   <h3>CHECK-OUT</h3>
-                  <p>10/10/10</p>
+                  <p>{ data.dateRange}</p>
                 </div>
               </div>
               <div className=" p-2">
