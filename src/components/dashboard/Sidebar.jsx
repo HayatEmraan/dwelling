dab8a777194460106ed71976cc5591db0e170e6e
nimairoy <nimairoy718@gmail.com>
2023-08-20 23:46:55 +0600

    solved sidebar issue

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -68,7 +68,7 @@ const Sidebar = () => {
     }
 
     return (
-        <div className='fixed top-0 dark:bg-slate-800 dark:text-white dark:h-full'>
+        <div className='fixed top-0 dark:lg:w-1/6 dark:w-3/4 dark:bg-slate-800 dark:text-white dark:h-full'>
             <div className='text-2xl font-semibold pt-3 pb-4 text-center '>Admin Panel</div>
             <div>
                 {
