e94b8ab6cf3554c62c096c51e04f2100c9029d56
nimairoy <nimairoy718@gmail.com>
2023-08-25 17:36:20 +0600

    updated select option's code

--- src/components/SelectOption/SelectOption.jsx
+++ src/components/SelectOption/SelectOption.jsx
@@ -2,15 +2,15 @@
 const SelectOption = () => {
 
     return (
-        <div>
+        <>
             <form>
-                <select defaultValue={'default'} className="select select-bordered w-full max-w-xs">
-                    <option selected value={'status'}>All Users</option>
-                    <option value={'active'}>Hosts</option>
-                    <option value={'blog'}>Guests</option>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option selected value={'allusers'}>All Users</option>
+                    <option value={'hosts'}>Hosts</option>
+                    <option value={'guests'}>Guests</option>
                 </select>
             </form>
-        </div>
+        </>
     );
 };
 
