78decd271ebf60f2cefa671b659c686cc3970b93
Armans07 <sikder89098@gmail.com>
2023-08-30 16:38:16 +0600

    added host list

--- src/app/dashboard/host/page.js
+++ src/app/dashboard/host/page.js
@@ -1,8 +1,16 @@
-const HostManagement = ()=>{
-    return(
-<div>
-    <h2>Host Page</h2>
-</div>
-    )
-}
-export default HostManagement
+
+import HostList from "@/components/dashboard/hostMangement/HostList";
+import HostTotal from "@/components/dashboard/hostMangement/HostTotal";
+
+
+
+const HostManagement = () => {
+  return (
+    <div className="m-5">
+      <h3 className="text-3xl pb-4 font-bold sm:px-6">Host Management</h3>
+      <HostTotal></HostTotal>
+      <HostList></HostList>
+    </div>
+  );
+};
+export default HostManagement;
---
+++ src/components/dashboard/hostMangement/HostTotal.jsx
@@ -0,0 +1,112 @@
+
+
+
+import React from "react";
+import { FiUsers } from "react-icons/fi";
+import { FiArrowUpRight } from "react-icons/fi";
+import { FiArrowDownRight } from "react-icons/fi";
+import { MdOutlineFolderDelete,MdPendingActions ,MdDoneAll} from "react-icons/md";
+
+
+const dashboardCards = [
+  {
+    id: 1,
+    title: "Host State",
+    icon: <FiUsers />,
+    project_detail: 18,
+    clientPercentage: "+9.18",
+    amount: "3156",
+    arrow: <FiArrowUpRight />,
+    bgColor: "#e1c38f",
+  },
+  {
+    id: 2,
+    title: "Declined",
+    icon: <MdOutlineFolderDelete />,
+    project_detail: 132,
+    clientPercentage: "-3.18",
+    amount: "167",
+    arrow: <FiArrowDownRight />,
+  },
+  {
+    id: 3,
+    title: "Pending",
+    icon: <MdPendingActions />,
+    project_detail: 12,
+    clientPercentage: "+183",
+    amount: "$41.56k",
+    arrow: <FiArrowUpRight />,
+  },
+  {
+    id: 4,
+    title: "Apporoved",
+    icon: <MdDoneAll />,
+    project_detail: "75%",
+    clientPercentage: "+6.18",
+    amount: "$33.16k",
+    arrow: <FiArrowUpRight />,
+  },
+];
+
+const HostTotal = () => {
+  return (
+    <div className="md:flex space-y-5 md:space-y-0 justify-between gap-4 items-center max-w-[150rem] sm:px-6 mt-5">
+      
+    {dashboardCards.map(
+      ({ id, title, icon, clientPercentage, amount, arrow, bgColor }) => {
+        return (
+          <div
+            key={id}
+            className="shadow-md p-4 w-full rounded-md bg-white transition-all hover:-translate-y-1"
+          >
+            <div className="flex justify-between items-center">
+              <span className="text-2xl rounded-md ">
+                <div
+                  className={`p-2 rounded ${
+                    title === "Clients"
+                      ? `bg-[${bgColor}] text-[#706f6e]`
+                      : title === "Invoices"
+                      ? "bg-[#a5f3fc] text-[#0891b2]"
+                      : title === "Sent Invoice"
+                      ? "bg-[#fecdd3] text-[#f43f5e]"
+                      : title === "Paid"
+                      ? "bg-[#e9d5ff] text-[#a855f7]"
+                      : ""
+                  }`}
+                >
+                  {icon}
+                </div>
+              </span>
+
+              <span></span>
+
+              <span className="rounded-md text-[#5d91eb] -2 flex">
+                <div
+                  className={` flex  ${
+                    clientPercentage === "+9.18"
+                      ? "text-green-500"
+                      : clientPercentage === "+183"
+                      ? " text-green-500"
+                      : clientPercentage === "+6.18"
+                      ? "text-green-500"
+                      : clientPercentage === "-3.18"
+                      ? "text-red-500 "
+                      : ""
+                  }`}
+                >
+                  {" "}
+                  <div className="mt-1">{arrow} </div> {clientPercentage}%
+                </div>
+              </span>
+            </div>
+            <p className="mt-5 text-slate-500">{title}</p>
+            <h2 className="text-2xl font-bold pt-5 pb-4">{amount}</h2>
+          </div>
+        );
+      }
+    )}
+  </div>
+  );
+};
+
+export default HostTotal;
