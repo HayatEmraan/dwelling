02f61cf8e6545021f9b443aa8f8c6b7a0459aed1
unknown <ChowdhuryJoyanti>
2023-08-31 15:23:36 +0600

    setting added

---
+++ src/components/dashboard/settingPage/test.js
@@ -0,0 +1,363 @@
+// {/* <div className="grid grid-cols-1 px-4 xl:grid-cols-2 xl:gap-4">
+// <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800 xl:mb-0">
+//   <div className="flow-root">
+//     <h3 className="text-xl font-semibold dark:text-white">
+//       Alerts &amp; Notifications
+//     </h3>
+//     <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+//       You can set up Themesberg to get notifications
+//     </p>
+//     <div className="divide-y divide-gray-200 dark:divide-gray-700">
+//       {/* Item 1 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Company News
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Get Themesberg news, announcements, and product updates
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="company-news"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="company-news"
+//             className="sr-only"
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 2 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Account Activity
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Get important notifications about you or activity you've
+//             missed
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="account-activity"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="account-activity"
+//             className="sr-only"
+//             defaultChecked=""
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 3 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Meetups Near You
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Get an email when a Dribbble Meetup is posted close to my
+//             location
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="meetups"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="meetups"
+//             className="sr-only"
+//             defaultChecked=""
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 4 */}
+//       <div className="flex items-center justify-between pt-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             New Messages
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Get Themsberg news, announcements, and product updates
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="new-messages"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="new-messages"
+//             className="sr-only"
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//     </div>
+//     <div className="mt-6">
+//       <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+//         Save all
+//       </button>
+//     </div>
+//   </div>
+// </div>
+// <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800 xl:mb-0">
+//   <div className="flow-root">
+//     <h3 className="text-xl font-semibold dark:text-white">
+//       Email Notifications
+//     </h3>
+//     <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+//       You can set up Themesberg to get email notifications{" "}
+//     </p>
+//     <div className="divide-y divide-gray-200 dark:divide-gray-700">
+//       {/* Item 1 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Rating reminders
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Send an email reminding me to rate an item a week after
+//             purchase
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="rating-reminders"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="rating-reminders"
+//             className="sr-only"
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 2 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Item update notifications
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Send user and product notifications for you
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="item-update"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="item-update"
+//             className="sr-only"
+//             defaultChecked=""
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 3 */}
+//       <div className="flex items-center justify-between py-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Item comment notifications
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Send me an email when someone comments on one of my items
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="item-comment"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input
+//             type="checkbox"
+//             id="item-comment"
+//             className="sr-only"
+//             defaultChecked=""
+//           />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//       {/* Item 4 */}
+//       <div className="flex items-center justify-between pt-4">
+//         <div className="flex flex-col flex-grow">
+//           <div className="text-lg font-semibold text-gray-900 dark:text-white">
+//             Buyer review notifications
+//           </div>
+//           <div className="text-base font-normal text-gray-500 dark:text-gray-400">
+//             Send me an email when someone leaves a review with their
+//             rating
+//           </div>
+//         </div>
+//         <label
+//           htmlFor="buyer-rev"
+//           className="relative flex items-center cursor-pointer"
+//         >
+//           <input type="checkbox" id="buyer-rev" className="sr-only" />
+//           <span className="h-6 bg-gray-200 border border-gray-200 rounded-full w-11 toggle-bg dark:bg-gray-700 dark:border-gray-600" />
+//         </label>
+//       </div>
+//     </div>
+//     <div className="mt-6">
+//       <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+//         Save all
+//       </button>
+//     </div>
+//   </div>
+// </div>
+// </div> */}
+
+
+
+
+
+   {/* <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+            <div className="flow-root">
+              <h3 className="text-xl font-semibold dark:text-white">
+                Other accounts
+              </h3>
+              <ul className="mb-6 divide-y divide-gray-200 dark:divide-gray-700">
+                <li className="py-4">
+                  <div className="flex justify-between xl:block 2xl:flex align-center 2xl:space-x-4">
+                    <div className="flex space-x-4 xl:mb-4 2xl:mb-0">
+                      <div>
+                        <img
+                          className="w-6 h-6 rounded-full"
+                          src="/images/users/bonnie-green.png"
+                          alt="Bonnie image"
+                        />
+                      </div>
+                      <div className="flex-1 min-w-0">
+                        <p className="text-base font-semibold text-gray-900 leading-none truncate mb-0.5 dark:text-white">
+                          Bonnie Green
+                        </p>
+                        <p className="mb-1 text-sm font-normal truncate text-primary-700 dark:text-primary-500">
+                          New York, USA
+                        </p>
+                        <p className="text-xs font-medium text-gray-500 dark:text-gray-400">
+                          Last seen: 1 min ago
+                        </p>
+                      </div>
+                    </div>
+                    <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
+                      <a
+                        href="#"
+                        className="w-full px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        Disconnect
+                      </a>
+                    </div>
+                  </div>
+                </li>
+                <li className="py-4">
+                  <div className="flex justify-between xl:block 2xl:flex align-center 2xl:space-x-4">
+                    <div className="flex space-x-4 xl:mb-4 2xl:mb-0">
+                      <div>
+                        <img
+                          className="w-6 h-6 rounded-full"
+                          src="/images/users/jese-leos.png"
+                          alt="Jese image"
+                        />
+                      </div>
+                      <div className="flex-1 min-w-0">
+                        <p className="text-base font-semibold text-gray-900 leading-none truncate mb-0.5 dark:text-white">
+                          Jese Leos
+                        </p>
+                        <p className="mb-1 text-sm font-normal truncate text-primary-700 dark:text-primary-500">
+                          California, USA
+                        </p>
+                        <p className="text-xs font-medium text-gray-500 dark:text-gray-400">
+                          Last seen: 2 min ago
+                        </p>
+                      </div>
+                    </div>
+                    <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
+                      <a
+                        href="#"
+                        className="w-full px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        Disconnect
+                      </a>
+                    </div>
+                  </div>
+                </li>
+                <li className="py-4">
+                  <div className="flex justify-between xl:block 2xl:flex align-center 2xl:space-x-4">
+                    <div className="flex space-x-4 xl:mb-4 2xl:mb-0">
+                      <div>
+                        <img
+                          className="w-6 h-6 rounded-full"
+                          src="/images/users/thomas-lean.png"
+                          alt="Thomas image"
+                        />
+                      </div>
+                      <div className="flex-1 min-w-0">
+                        <p className="text-base font-semibold text-gray-900 leading-none truncate mb-0.5 dark:text-white">
+                          Thomas Lean
+                        </p>
+                        <p className="mb-1 text-sm font-normal truncate text-primary-700 dark:text-primary-500">
+                          Texas, USA
+                        </p>
+                        <p className="text-xs font-medium text-gray-500 dark:text-gray-400">
+                          Last seen: 1 hour ago
+                        </p>
+                      </div>
+                    </div>
+                    <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
+                      <a
+                        href="#"
+                        className="w-full px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        Disconnect
+                      </a>
+                    </div>
+                  </div>
+                </li>
+                <li className="pt-4">
+                  <div className="flex justify-between xl:block 2xl:flex align-center 2xl:space-x-4">
+                    <div className="flex space-x-4 xl:mb-4 2xl:mb-0">
+                      <div>
+                        <img
+                          className="w-6 h-6 rounded-full"
+                          src="/images/users/lana-byrd.png"
+                          alt="Lana image"
+                        />
+                      </div>
+                      <div className="flex-1 min-w-0">
+                        <p className="text-base font-semibold text-gray-900 leading-none truncate mb-0.5 dark:text-white">
+                          Lana Byrd
+                        </p>
+                        <p className="mb-1 text-sm font-normal truncate text-primary-700 dark:text-primary-500">
+                          Texas, USA
+                        </p>
+                        <p className="text-xs font-medium text-gray-500 dark:text-gray-400">
+                          Last seen: 1 hour ago
+                        </p>
+                      </div>
+                    </div>
+                    <div className="inline-flex items-center w-auto xl:w-full 2xl:w-auto">
+                      <a
+                        href="#"
+                        className="w-full px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        Disconnect
+                      </a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <div>
+                <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                  Save all
+                </button>
+              </div>
+            </div>
+          </div> */}
