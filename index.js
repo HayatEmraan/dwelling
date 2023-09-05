3cb0f1669de84a40bf0639ed8b127aa3b4750ba1
hayatemraan <emraanhasanhayat@gmail.com>
2023-09-05 15:08:29 +0600

    'update

--- index.js
+++
@@ -1,7 +0,0 @@
-const email = "emraanhasanhayat@gmail.com";
-const obscuredEmail = email.replace(
-  /^(.{3}).*?(?=@)/,
-  (_, firstThreeChars) => firstThreeChars + "*".repeat(5)
-);
-
-console.log(obscuredEmail);
