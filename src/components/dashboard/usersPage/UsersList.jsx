837b205051d5234f4229fc20e7d6dcbc81e9bf71
raju720190r <raju720190r@gmail.com>
2023-09-06 19:07:15 +0600

    Some changes

--- src/components/dashboard/usersPage/UsersList.jsx
+++ src/components/dashboard/usersPage/UsersList.jsx
@@ -28,9 +28,9 @@ const UsersList = ({ data }) => {
                     </p>
                   </div>
                    {/* filter and search */}
-                  <div className="grid lg:grid-cols-2 gap-3 lg:flex-end">
+                  <div className="inline-flex">
                     {/* Search Bar */}
-                    <div className="grid gap-3 md:flex md:justify-between md:items-center border-gray-200 dark:border-gray-700">
+                    <div className="lg:mr-3 md:flex md:justify-between md:items-center border-gray-200 dark:border-gray-700">
                       {/* Input */}
                       <div className="sm:col-span-1">
                         <label
@@ -272,7 +272,7 @@ const UsersList = ({ data }) => {
                                   >
                                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                                   </svg>
-                                  Unblock
+                                  blocked
                                 </span>
                               )}
                             </div>
