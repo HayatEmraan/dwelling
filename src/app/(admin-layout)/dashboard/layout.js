1a18567a10cd5bca5b8a5f56953eb22ec8564801
hayatemraan <emraanhasanhayat@gmail.com>
2023-09-10 16:45:20 +0600

    updated

--- src/app/(admin-layout)/dashboard/layout.js
+++ src/app/(admin-layout)/dashboard/layout.js
@@ -10,12 +10,6 @@ const DashboardLayout = ({ children }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-6">
       <div
-        // className={`h-screen  transition-all  ${
-        //   isOpen
-        //     ? "hidden"
-        //     : "block z-30 left-0 top-10 w-3/4 h-[100vh] fixed md:static"
-        // }`}
-
         className={`fixed top-0 left-0  z-20 lg:flex bg-white flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 transition-width ${
           isOpen ? "" : "hidden"
         }`}
