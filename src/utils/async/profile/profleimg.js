d0b047d791203729a0485ad2ff7291348b7f36ae
hayatemraan <emraanhasanhayat@gmail.com>
2023-09-12 23:36:56 +0600

    updated profile image apis

--- src/utils/async/profile/profleimg.js
+++ src/utils/async/profile/profleimg.js
@@ -1,6 +1,6 @@
 "use server";
 import { cookies } from "next/headers";
-export async function profileimg(id) {
+export async function profileimg(id, photoURL) {
   const res = await fetch(
     `https://dwelling-bright-server.vercel.app/api/v2/user/updateimg?id=${id}`,
     {
@@ -9,6 +9,9 @@ export async function profileimg(id) {
         "Content-Type": "application/json",
         authorization: `Bearer ${cookies().get("accessToken")?.value}`,
       },
+      body: JSON.stringify({
+        photoURL,
+      }),
     }
   );
   return await res.json();
