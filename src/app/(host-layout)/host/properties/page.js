650eeb0e411a03c4457a7ff4401bd9483f79c071
Armans07 <sikder89098@gmail.com>
2023-09-13 20:42:23 +0600

    added host property

--- src/app/(host-layout)/host/properties/page.js
+++ src/app/(host-layout)/host/properties/page.js
@@ -1,9 +1,15 @@
+import HostPropertyTotal from '@/components/host/HostPropertyTotal';
+import PropertyHost from '@/components/host/PropertyHost';
+import { getproperties } from '@/utils/async/admin/properties/getproperties';
 import React from 'react';
 
-const PropertiesHost = () => {
+const PropertiesHost = async () => {
+    const data = await getproperties();
     return (
-        <div>
-            <h1>this is properties host</h1>
+        <div className="m-5">
+           <h2 className="text-3xl pb-4 font-bold sm:px-6">Property Management</h2>
+           <HostPropertyTotal></HostPropertyTotal>
+           <PropertyHost data={data}></PropertyHost>
         </div>
     );
 };
