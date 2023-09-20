20b0dd4bd5d728b13b65357deeb61e9ba6eb09d8
unknown <ChowdhuryJoyanti>
2023-09-20 10:49:30 +0600

    our team page added

---
+++ src/app/our-team/page.js
@@ -0,0 +1,13 @@
+import Team from '@/components/ourTeam/Team';
+import React from 'react';
+
+const OurTeamPage = () => {
+    return (
+        <div className='mt-5'>
+          
+             <Team></Team>
+        </div>
+    );
+};
+
+export default OurTeamPage;
