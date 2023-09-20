a8c8173b147df2f7da931d1e0af88612c9474443
hayatemraan <emraanhasanhayat@gmail.com>
2023-09-20 11:44:43 +0600

    updated middleware'

--- src/middleware.js
+++ src/middleware.js
@@ -3,13 +3,30 @@ import { middleutils } from "./utils/helpers/middleutlis";
 
 export async function middleware(req, res, next) {
   const verifyCookies = await middleutils(req);
-  if (verifyCookies.msg === "Success") {
-    return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/", req.url));
+  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    if (
+      verifyCookies.msg === "Success" &&
+      verifyCookies.data.role === "admin"
+    ) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else if (req.nextUrl.pathname.startsWith("/host")) {
+    if (verifyCookies.msg === "Success" && verifyCookies.data.role === "host") {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else if (req.nextUrl.pathname.startsWith("/guest")) {
+    if (verifyCookies.msg === "Success" && verifyCookies.data.role === "user") {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/host/:path*", "/guest/:path*"],
 };
