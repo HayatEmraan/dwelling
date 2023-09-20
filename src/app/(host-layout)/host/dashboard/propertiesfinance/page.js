7caacbffe05a1239aebab4ec05c1d4b24b0dfdb7
hayatemraan <emraanhasanhayat@gmail.com>
2023-09-21 00:05:10 +0600

    'updated

--- src/app/(host-layout)/host/dashboard/propertiesfinance/page.js
+++ src/app/(host-layout)/host/dashboard/propertiesfinance/page.js
@@ -1,16 +1,13 @@
-import Example from "@/components/dashboard/homePage/DashPieChart";
-import PropertyManagement from "@/components/dashboard/propertyPage/PropertyManagement";
 import PropertyTotal from "@/components/dashboard/propertyPage/PropertyTotal";
 import PropertiesFinanceTable from "@/components/dashboard/propertyPage/propertyFinance/PropertiesFinanceTable";
 import PropertyFinanceBarChart from "@/components/dashboard/propertyPage/propertyFinance/PropertyFinanceBarChart";
 import PropertyFinanceLineChart from "@/components/dashboard/propertyPage/propertyFinance/PropertyFinanceLineChart";
-import HostPropertyChart from "@/components/host/HostPropertyChart";
-import { getproperties } from "@/utils/async/admin/properties/getproperties";
+import { getrooms } from "@/utils/async/host/getrooms";
 
 import React from "react";
 
 const PropertiesFinance = async () => {
-  const data = await getproperties();
+  const data = await getrooms();
   return (
     <div className="m-5">
       <div className="mx-auto items-center mt-5">
