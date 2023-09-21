ac5dc416f6c6656d32e4fd1d97d4bdaf8855c8f2
mehedi001 <mh.hridoy5070@gmail.com>
2023-09-21 17:19:57 +0600

    fast

--- src/components/guest/dashboard/bookingPage/Refund.jsx
+++ src/components/guest/dashboard/bookingPage/Refund.jsx
@@ -81,13 +81,13 @@ const Refund = ({ id }) => {
               </form>
             </div>
             <div className="flex justify-center items-center gap-x-2 py-4 px-4 dark:border-gray-700">
-              {/* <button
+              <button
                 type="button"
                 className="hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
                 data-hs-overlay="#hs-basic-modal"
               >
                 Close
-              </button> */}
+              </button>
               
               <ReScheduleModal />
             </div>
